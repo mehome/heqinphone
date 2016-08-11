225e1ff444f7b7f2afaba20df90747642b3776b6
zoujing01 <zoujing01@baidu.com>
2016-08-11 23:42:36 +0800

    这里用来处理，接收会议时的操作，以便存储当前的会议，然后转入到会议中，进入会议中，会去请求当前的会议类型

--- linphone-iphone_with_3.9.1sdk/Classes/LinphoneManager.m
+++ linphone-iphone_with_3.9.1sdk/Classes/LinphoneManager.m
@@ -1935,6 +1935,26 @@ static int comp_call_state_paused(const LinphoneCall *call, const void *param) {
 #pragma mark - Call Functions
 
 - (void)acceptCall:(LinphoneCall *)call evenWithVideo:(BOOL)video {
+    
+    /*** 下面这部分代码应该放在accept会议时，用来接听会议***/
+//    const LinphoneAddress *currentRemoteAddress = linphone_core_get_current_call_remote_address(LC);
+//    const LinphoneAddress *remoteAddress =  linphone_call_get_remote_address(call);
+//    
+//    char *remoteAddStr = linphone_call_get_remote_address_as_string(call);          // "qin.he" <sip:qin.he@zijingcloud.com>
+//    
+//    NSString *displayName0 = [FastAddressBook displayNameForAddress:currentRemoteAddress];      // qin.he
+//    char *caddr0 = linphone_address_as_string(currentRemoteAddress);                        // "qin.he" <sip:qin.he@zijingcloud.com>
+//    
+//    NSString *displayName1 = [FastAddressBook displayNameForAddress:remoteAddress];         // qin.he
+//    char *caddr1 = linphone_address_as_string(remoteAddress);                               // "qin.he" <sip:qin.he@zijingcloud.com>
+//    
+//    NSLog(@"remoteAddStr=%s, displayName0=%@, caddr0=%s, displayName1=%@, caddr1=%s",
+//          remoteAddStr, displayName0, caddr0, displayName1, caddr1);
+//    // 这里应该赋值当前的会议情况
+//    [LPSystemUser sharedUser].curMeetingAddr = callStr;         // 希望会是直接qin.he@zijingcloud.com
+    // 上面这部分代码用来存储信息，以便在会议中进行处理
+    
+    
 	LinphoneCallParams *lcallParams = linphone_core_create_call_params(theLinphoneCore, call);
 	if (!lcallParams) {
 		LOGW(@"Could not create call parameters for %p, call has probably already ended.", call);
